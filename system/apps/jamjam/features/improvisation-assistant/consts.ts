934c7fd0202af1d94a377e3d11f49d784580dd75
Adrian Połubiński <adrian.polubinski@billennium.com>
2023-05-15 09:49:54 +0200

    29. Create modeling for music theory

---
+++ system/apps/jamjam/features/improvisation-assistant/consts.ts
@@ -0,0 +1,79 @@
+//                C, C#, D, D#, E, F, F#, G, G#, A, A#, B
+const NOTE_IDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] as const;
+
+const NOTE_NOTATIONS = ['sharp', 'bmoll'] as const;
+
+const SHARP_NOTE_NOTATION_SYMBOLS = [
+  'C',
+  'C#',
+  'D',
+  'D#',
+  'E',
+  'F',
+  'F#',
+  'G',
+  'G#',
+  'A',
+  'A#',
+  'B',
+] as const;
+
+const BMOLL_NOTE_NOTATION_SYMBOLS = [
+  'C',
+  'Db',
+  'D',
+  'Eb',
+  'E',
+  'F',
+  'Gb',
+  'G',
+  'Ab',
+  'A',
+  'Bb',
+  'B',
+] as const;
+
+const NOTE_OCTAVES = [0, 1, 2, 3, 4, 5, 6, 7, 8] as const;
+
+const GUITAR_STRINGS_NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] as const;
+
+const GUITAR_FRETS = [
+  'empty',
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  13,
+  14,
+  15,
+  16,
+  17,
+  18,
+  19,
+  20,
+  21,
+  22,
+  23,
+  24,
+  25,
+  26,
+  27,
+] as const;
+
+export {
+  NOTE_IDS,
+  NOTE_OCTAVES,
+  GUITAR_STRINGS_NUMBERS,
+  GUITAR_FRETS,
+  SHARP_NOTE_NOTATION_SYMBOLS,
+  BMOLL_NOTE_NOTATION_SYMBOLS,
+  NOTE_NOTATIONS,
+};
---
+++ system/apps/jamjam/features/improvisation-assistant/tests/consts.test.ts
@@ -0,0 +1,94 @@
+import {
+  BMOLL_NOTE_NOTATION_SYMBOLS,
+  GUITAR_FRETS,
+  GUITAR_STRINGS_NUMBERS,
+  NOTE_IDS,
+  NOTE_NOTATIONS,
+  NOTE_OCTAVES,
+  SHARP_NOTE_NOTATION_SYMBOLS,
+} from '../consts';
+
+describe('initial configuration refers to reality when', () => {
+  it('there is a sharp and bmoll notation', () => {
+    expect(NOTE_NOTATIONS).toEqual(['sharp', 'bmoll']);
+  });
+
+  it('we only use notes from standard music', () => {
+    expect(NOTE_IDS).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+  });
+
+  it('sharp notation have symbols from music theory', () => {
+    expect(SHARP_NOTE_NOTATION_SYMBOLS).toEqual([
+      'C',
+      'C#',
+      'D',
+      'D#',
+      'E',
+      'F',
+      'F#',
+      'G',
+      'G#',
+      'A',
+      'A#',
+      'B',
+    ]);
+  });
+
+  it('bmoll notation have symbols from music theory', () => {
+    expect(BMOLL_NOTE_NOTATION_SYMBOLS).toEqual([
+      'C',
+      'Db',
+      'D',
+      'Eb',
+      'E',
+      'F',
+      'Gb',
+      'G',
+      'Ab',
+      'A',
+      'Bb',
+      'B',
+    ]);
+  });
+
+  it('there are only nine supported octaves (0 is counted in)', () => {
+    expect(NOTE_OCTAVES).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8]);
+  });
+
+  it('guitar can have only from 1 to 10 strings', () => {
+    expect(GUITAR_STRINGS_NUMBERS).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  });
+
+  it('there are 27 guitar frets supported and also there is a empty string', () => {
+    expect(GUITAR_FRETS).toEqual([
+      'empty',
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      11,
+      12,
+      13,
+      14,
+      15,
+      16,
+      17,
+      18,
+      19,
+      20,
+      21,
+      22,
+      23,
+      24,
+      25,
+      26,
+      27,
+    ]);
+  });
+});
