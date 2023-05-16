40eddf297e50dc00ea8444cfb73c6ce9cee5028e
Adrian Połubiński <adrian.polubinski@billennium.com>
2023-05-16 10:34:25 +0200

    34. Add fix for fonts

--- system/apps/jamjam/next.config.js
+++ system/apps/jamjam/next.config.js
@@ -1,7 +1,23 @@
 //@ts-check
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
 const { withNx } = require('@nrwl/next/plugins/with-nx');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const copyFonts = (/** @type {{ plugins: CopyWebpackPlugin[]; }} */ config) => {
+  config.plugins.push(
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.join(__dirname, '../../assets/fonts'),
+          to: path.join(__dirname, 'public/fonts'),
+        },
+      ],
+    })
+  );
+};
 
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
@@ -11,6 +27,13 @@ const nextConfig = {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: false,
+  },
+  webpack: (config, { isServer }) => {
+    // Important: return the modified config
+
+    copyFonts(config);
+
+    return config;
   },
 };
 
