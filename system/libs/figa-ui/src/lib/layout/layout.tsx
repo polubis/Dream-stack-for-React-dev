1ab92de4e8cfbee5ec23714d979253ae7f432759
Adrian Połubiński <adrian.polubinski@billennium.com>
2023-05-19 08:51:23 +0200

    45. Improve Layout component/fonts/modal

--- system/libs/figa-ui/src/lib/layout/layout.tsx
+++ system/libs/figa-ui/src/lib/layout/layout.tsx
@@ -1,10 +1,11 @@
+import { ReactNode } from 'react';
 import type { LayoutProps } from './defs';
 
 import c from 'classnames';
 
-const Layout = ({ className, children, header, footer }: LayoutProps) => {
+const Layout = ({ className, children, header, footer, full }: LayoutProps) => {
   return (
-    <div className={c('layout', className)}>
+    <div className={c('layout', className, { full })}>
       <header className="layout-header">{header}</header>
       <main className="layout-content">{children}</main>
       <footer className="layout-footer">{footer}</footer>
