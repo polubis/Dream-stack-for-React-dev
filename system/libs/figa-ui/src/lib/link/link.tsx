61ee3fa7d883755154f45e9a03c7eacd0190a164
polubis <polubis@wp.pl>
2023-05-20 10:31:01 +0200

    51. Add option to update nx cloud if error

--- system/libs/figa-ui/src/lib/link/link.tsx
+++ system/libs/figa-ui/src/lib/link/link.tsx
@@ -3,9 +3,18 @@ import type { LinkProps } from './defs';
 import c from 'classnames';
 import { Font } from '../font';
 
-const Link = ({ className, variant, children, motive = 'default' }: LinkProps) => {
+const Link = ({
+  className,
+  variant,
+  children,
+  motive = 'default',
+}: LinkProps) => {
   return (
-    <Font className={c('link', className, motive)} variant={variant} element="span">
+    <Font
+      className={c('link', className, motive)}
+      variant={variant}
+      element="span"
+    >
       {children}
     </Font>
   );
