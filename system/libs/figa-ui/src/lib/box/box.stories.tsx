90e302c9eab651858f96fad40617091172824d83
polubis <polubis@wp.pl>
2023-05-21 10:34:23 +0200

    52. Add margins handling for Box component

--- system/libs/figa-ui/src/lib/box/box.stories.tsx
+++ system/libs/figa-ui/src/lib/box/box.stories.tsx
@@ -1,3 +1,4 @@
+import type { ReactNode } from 'react';
 import type { Story, Meta } from '@storybook/react';
 
 import { Box } from './box';
@@ -9,63 +10,98 @@ export default {
   title: 'Box',
 } as Meta;
 
-const Template: Story = () => {
+const Template: Story<{ children: ReactNode }> = (props) => {
   return (
     <div
       style={{ display: 'flex', flexFlow: 'column', gap: 12, padding: '24px' }}
     >
-      <Box orientation="row">
-        <Font variant="h6">My header</Font>
-        <Font variant="b1">My header content</Font>
-        <Button>Some button</Button>
-      </Box>
-      <Box>
-        <Font variant="h6">My header</Font>
-        <Font variant="b1">My header content</Font>
-        <Button>Some button</Button>
-      </Box>
-      <Box orientation="center-column">
-        <Font variant="h6">My header</Font>
-        <Font variant="b1">My header content</Font>
-        <Button>Some button</Button>
-      </Box>
-      <Box orientation="center-row">
-        <Font variant="h6">My header</Font>
-        <Font variant="b1">My header content</Font>
-        <Button>Some button</Button>
-      </Box>
-      <Box orientation="center-row" variant="outlined">
-        <Font variant="h6">My header</Font>
-        <Font variant="b1">My header content</Font>
-        <Button>Some button</Button>
-      </Box>
-      <Box orientation="center-row" variant="filled">
-        <Font variant="h6">My header</Font>
-        <Font variant="b1">My header content</Font>
-        <Button>Some button</Button>
-      </Box>
-      <Box
-        orientation="center-row"
-        variant="filled"
-        padding={[100, 100, 100, 100]}
-      >
-        <Font variant="h6">My header</Font>
-        <Font variant="b1">My header content</Font>
-        <Button>Some button</Button>
-      </Box>
-      <Box
-        orientation="center-column"
-        variant="filled"
-        padding={[100, 100, 100, 100]}
-        spacing={[1000, 500, 1000]}
-      >
-        <Font variant="h6">My header</Font>
-        <Font variant="b1">My header content</Font>
-        <Button>Some button</Button>
-      </Box>
+      {props.children}
     </div>
   );
 };
 
-export const Default = Template.bind({});
-Default.args = {};
+export const OrientationAndSpacing = Template.bind({});
+OrientationAndSpacing.args = {
+  children: (
+    <>
+      <Box orientation="row" spacing={[100, 150, 200]}>
+        <Font variant="h6">My header</Font>
+        <Font variant="b1">My header content</Font>
+        <Button>Some button</Button>
+      </Box>
+      <Box spacing={[100, 150, 200]}>
+        <Font variant="h6">My header</Font>
+        <Font variant="b1">My header content</Font>
+        <Button>Some button</Button>
+      </Box>
+      <Box orientation="center-column" spacing={[100, 150, 200]}>
+        <Font variant="h6">My header</Font>
+        <Font variant="b1">My header content</Font>
+        <Button>Some button</Button>
+      </Box>
+      <Box orientation="center-row" spacing={[100, 150, 200]}>
+        <Font variant="h6">My header</Font>
+        <Font variant="b1">My header content</Font>
+        <Button>Some button</Button>
+      </Box>
+    </>
+  ),
+};
+
+export const Padding = Template.bind({});
+Padding.args = {
+  children: (
+    <Box spacing={[100, 150, 200]} padding={[950, 950, 950, 950]}>
+      <Font variant="h6">My header</Font>
+      <Font variant="b1">My header content</Font>
+      <Button>Some button</Button>
+    </Box>
+  ),
+};
+
+export const Margins = Template.bind({});
+Margins.args = {
+  children: (
+    <>
+      <div>
+        <Box spacing={[100, 150, 200]} margin="auto">
+          <Font variant="h6">My header</Font>
+          <Font variant="b1">My header content</Font>
+          <Button>Some button</Button>
+        </Box>
+      </div>
+      <div>
+        <Box spacing={[100, 150, 200]} margin={['auto', 300, 150, 'auto']}>
+          <Font variant="h6">My header</Font>
+          <Font variant="b1">My header content</Font>
+          <Button>Some button</Button>
+        </Box>
+      </div>
+      <div>
+        <Box spacing={[100, 150, 200]} margin={[500, 500, 500, 500]}>
+          <Font variant="h6">My header</Font>
+          <Font variant="b1">My header content</Font>
+          <Button>Some button</Button>
+        </Box>
+      </div>
+    </>
+  ),
+};
+
+export const Variants = Template.bind({});
+Variants.args = {
+  children: (
+    <>
+      <Box variant="outlined" spacing={[100, 150, 200]}>
+        <Font variant="h6">My header</Font>
+        <Font variant="b1">My header content</Font>
+        <Button>Some button</Button>
+      </Box>
+      <Box variant="filled" spacing={[100, 150, 200]}>
+        <Font variant="h6">My header</Font>
+        <Font variant="b1">My header content</Font>
+        <Button>Some button</Button>
+      </Box>
+    </>
+  ),
+};
--- system/libs/figa-ui/src/lib/box/box.test.tsx
+++ system/libs/figa-ui/src/lib/box/box.test.tsx
@@ -27,7 +27,7 @@ describe('User is able to use box when', () => {
     expect(asFragment()).toMatchSnapshot();
   });
 
-  it('[FRAGILE] creates space', () => {
+  it('[FRAGILE] creates space for rows', () => {
     const { asFragment } = render(
       <Box orientation="center-row" spacing={[100, 150, 200]}>
         <div>1</div>
@@ -37,6 +37,56 @@ describe('User is able to use box when', () => {
     );
 
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('[FRAGILE] creates space for columns', () => {
+    const { asFragment } = render(
+      <Box spacing={[100, 150, 200]}>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </Box>
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  describe('applies margins', () => {
+    it('[FRAGILE] for auto', () => {
+      const { container } = render(
+        <Box orientation="column" margin="auto">
+          <div>1</div>
+        </Box>
+      );
+
+      expect(
+        (container.querySelector('.box') as HTMLElement).style.margin
+      ).toBe('auto');
+    });
+
+    it('[FRAGILE] for mixed setup', () => {
+      const { container } = render(
+        <Box orientation="column" margin={['auto', 50, 50, 50]}>
+          <div>1</div>
+        </Box>
+      );
+
+      expect(
+        (container.querySelector('.box') as HTMLElement).style.margin
+      ).toBe('auto 4px 4px 4px');
+    });
+  });
+
+  it('[FRAGILE] skipps margins apply when not passed', () => {
+    const { container } = render(
+      <Box orientation="column">
+        <div>1</div>
+      </Box>
+    );
+
+    expect((container.querySelector('.box') as HTMLElement).style.margin).toBe(
+      ''
+    );
   });
 
   it('[FRAGILE] allows to use padding and assigns custom classes', () => {
