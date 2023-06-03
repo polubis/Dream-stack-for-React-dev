12339e5dfcd4cdea7e516f976fbb3956471e485a
Artur Węgrodzki <artur.wegrodzki@gmail.com>
2023-06-03 09:55:26 +0200

    Fix cors and set snippet gif url as optional

---
+++ dotnet/GreenOnSoftware/GreenOnSoftware.DataAccess/Migrations/20230603075206_SetSnippetGifUrlOptional.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GreenOnSoftware.DataAccess.Migrations
+{
+    public partial class SetSnippetGifUrlOptional : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "GifUrl",
+                table: "Snippets",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(300)",
+                oldMaxLength: 300);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "GifUrl",
+                table: "Snippets",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(300)",
+                oldMaxLength: 300,
+                oldNullable: true);
+        }
+    }
+}
