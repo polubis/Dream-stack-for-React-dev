def18625788abf5c63bbb2f4f24934ec32bf487e
polubis <polubis@wp.pl>
2023-06-08 08:26:55 +0200

    98. Enhance fonts component with italic option

--- system/libs/figa-ui/src/lib/font/font.stories.tsx
+++ system/libs/figa-ui/src/lib/font/font.stories.tsx
@@ -3,6 +3,7 @@ import type { FontProps } from './defs';
 
 import { Font } from './font';
 import { FONT_VARIANTS } from './consts';
+import { Box } from '../box';
 
 export default {
   component: Font,
@@ -11,13 +12,23 @@ export default {
 
 const Template: Story<FontProps> = (args) => {
   return (
-    <>
-      {FONT_VARIANTS.map((variant) => (
-        <div key={variant} style={{ margin: '8px 0' }}>
-          <Font {...args} variant={variant} children={variant} />
-        </div>
-      ))}
-    </>
+    <Box orientation="row">
+      <Box>
+        {FONT_VARIANTS.map((variant) => (
+          <div key={variant} style={{ margin: '8px 0' }}>
+            <Font {...args} variant={variant} children={variant} />
+          </div>
+        ))}
+      </Box>
+
+      <Box>
+        {FONT_VARIANTS.map((variant) => (
+          <div key={'italic' + variant} style={{ margin: '8px 0' }}>
+            <Font {...args} italic variant={variant} children={variant} />
+          </div>
+        ))}
+      </Box>
+    </Box>
   );
 };
 
