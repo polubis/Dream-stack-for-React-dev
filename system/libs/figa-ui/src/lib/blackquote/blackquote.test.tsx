923e5fba63ffc16c79c1b74171981c00fec0a58c
Szymon Kedra <s5zee@wp.pl>
2023-06-14 10:28:05 +0200

    updated a description for test to better match other files description

--- system/libs/figa-ui/src/lib/blackquote/blackquote.test.tsx
+++ system/libs/figa-ui/src/lib/blackquote/blackquote.test.tsx
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react';
 import { Blackquote } from './blackquote';
 
-describe('Blackquote', () => {
+describe('There is option to use blackquote when', () => {
   it('renders content', () => {
     render(<Blackquote title="Example text for testing." />);
   });
