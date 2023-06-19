7e9bbf2740d9f2fb381db953ce35802e89008189
polubis <polubis@wp.pl>
2023-06-19 08:36:34 +0200

    108. Add client/server side detection utils

---
+++ system/libs/utils/src/lib/tests/is-client.test.ts
@@ -0,0 +1,7 @@
+import { isClient } from '../is-client';
+
+describe('Client side is detected when: ', () => {
+  it('window object is not equal to undefined', () => {
+    expect(isClient()).toBe(true);
+  });
+});
