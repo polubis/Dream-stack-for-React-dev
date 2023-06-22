7f4dedae0c742af550a6ce9c072f2d239acad2d2
polubis <polubis@wp.pl>
2023-06-22 14:55:13 +0200

    Implement sign in view

---
+++ system/apps/blog/views/sign-in/sign-in.view.tsx
@@ -0,0 +1,11 @@
+import { MainLayout, SignInForm } from '../../components';
+
+const SignInView = () => {
+  return (
+    <MainLayout className="sign-in-view">
+      <SignInForm />
+    </MainLayout>
+  );
+};
+
+export { SignInView };
