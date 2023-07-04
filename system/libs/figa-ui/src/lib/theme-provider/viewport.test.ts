2f278d25a6f386656a2ab91faf2c5c071f137c3c
Adrian Połubiński <adrian.polubinski@billennium.com>
2023-07-04 13:30:55 +0200

    141. Improve viewport functions and tests

--- system/libs/figa-ui/src/lib/theme-provider/viewport.test.ts
+++ system/libs/figa-ui/src/lib/theme-provider/viewport.test.ts
@@ -1,11 +1,31 @@
-import { getDownViewport, getUpViewport } from './viewport';
+import {
+  getDownViewport,
+  getUpViewport,
+  isSmUp,
+  isSmDown,
+  VIEWPORT,
+} from './viewport';
 
-describe('Creates viewport when', () => {
+describe('Creates viewport when: ', () => {
   it('up media query is created based on given value', () => {
     expect(getUpViewport(100)).toBe('(min-width: 100px)');
   });
 
   it('down media query is created with one pixel less', () => {
     expect(getDownViewport(100)).toBe('(max-width: 99px)');
+  });
+
+  it('detects viewports with given functions', () => {
+    expect(isSmUp(421)).toBe(true);
+    expect(isSmUp(420)).toBe(true);
+    expect(isSmUp(419)).toBe(false);
+
+    expect(isSmDown(421)).toBe(false);
+    expect(isSmDown(420)).toBe(false);
+    expect(isSmDown(419)).toBe(true);
+  });
+
+  it('viewport object have supported values', () => {
+    expect(VIEWPORT).toMatchSnapshot();
   });
 });
