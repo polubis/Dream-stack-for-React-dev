64eeece9d8e2c5e6b90a8c4073eefd2d789a9e89
Artur Węgrodzki <Reynart01@users.noreply.github.com>
2023-07-18 07:49:57 +0200

    172. Articles searching per lang (#238)

--- dotnet/GreenOnSoftware/GreenOnSoftware.Application/Articles/GetArticlesQuery/GetArticles.cs
+++ dotnet/GreenOnSoftware/GreenOnSoftware.Application/Articles/GetArticlesQuery/GetArticles.cs
@@ -1,6 +1,17 @@
 ﻿using GreenOnSoftware.Commons.CQRS;
+using GreenOnSoftware.Core.Enums;
 
 namespace GreenOnSoftware.Application.Articles.GetArticlesQuery;
 
 public record GetArticles(string[]? Status, string? Search, int? ItemsPerPage, int? CurrentPage) 
-    : ISearchQuery<ArticleLookupDto>;
+    : ISearchQuery<ArticleLookupDto>
+{
+    internal Language Lang { get; private set; }
+
+    public GetArticles Bind(Language lang)
+    {
+        Lang = lang;
+
+        return this;
+    }
+}
