b754382212f31c8d1bb2ddf5fcb07382ef6c97e3
Adrian Połubiński <adrian.polubinski@billennium.com>
2023-08-16 09:34:27 +0200

    180. [PATCH] Remove assertion and replace it with type guard in useClickOutside

--- system/libs/figa-hooks/src/lib/use-click-outside/use-click-outside.ts
+++ system/libs/figa-hooks/src/lib/use-click-outside/use-click-outside.ts
@@ -2,22 +2,17 @@ import type { UseClickOutsideConfig } from './defs';
 
 import { useEffect, useRef } from 'react';
 
-/**
- * Hook responsible for detecting the moment
- * when the user is clicking outside any HTML element.
- *
- * @param {UseClickOutsideConfig} - Configuration object.
- * @returns {UseClickOutsideReturn} - API that any component will use.
- */
+const assertIsNode = (target: EventTarget | Node | null): target is Node =>
+  !!target && 'nodeType' in target;
+
 const useClickOutside = <T extends HTMLElement>({
   onOutside,
 }: UseClickOutsideConfig) => {
-  // Stores a reference to the HTML element that will be tracked.
   const ref = useRef<T>(null);
 
   useEffect(() => {
-    const handleClickOutside = (e: MouseEvent) => {
-      ref.current?.contains(e.target as Node) || onOutside();
+    const handleClickOutside = ({ target }: MouseEvent) => {
+      (assertIsNode(target) && ref.current?.contains(target)) || onOutside();
     };
 
     document.addEventListener('mousedown', handleClickOutside);
