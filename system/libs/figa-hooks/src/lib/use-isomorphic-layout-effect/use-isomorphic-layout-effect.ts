f8a0b1ba363897cd92d38e68ca53abcf0a5a91a7
polubis <polubis@wp.pl>
2023-09-15 07:35:03 +0200

    208. [PATCH] Add useSSRSafeEffect

--- system/libs/figa-hooks/src/lib/use-isomorphic-layout-effect/use-isomorphic-layout-effect.ts
+++ system/libs/figa-hooks/src/lib/use-isomorphic-layout-effect/use-isomorphic-layout-effect.ts
@@ -2,5 +2,7 @@ import { isClient } from '@system/utils';
 import { useEffect, useLayoutEffect } from 'react';
 
 const useIsomorphicLayoutEffect = isClient() ? useLayoutEffect : useEffect;
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+const useSSRSafeEffect = isClient() ? useLayoutEffect : () => {};
 
-export { useIsomorphicLayoutEffect };
+export { useIsomorphicLayoutEffect, useSSRSafeEffect };
