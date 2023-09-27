84cad54bd059bb853e6f85e02c4d14745d5e3620
Adrian Połubiński <polubis@wp.pl>
2023-09-27 09:08:33 +0200

    219. [PATCH] Fix fonts (#285)
    Co-authored-by: Adrian Połubiński <adrian.polubinski@billennium.com>

--- system/apps/blog/pages/_app.tsx
+++ system/apps/blog/pages/_app.tsx
@@ -2,42 +2,31 @@ import { ThemeProvider } from '@system/figa-ui';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { useAuth } from '../core';
+import { useEffect } from 'react';
 
 const App = ({ Component, pageProps }: AppProps) => {
   useAuth();
 
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/service-worker.js')
+        .then((registration) => {
+          console.log(
+            'Service Worker registered with scope:',
+            registration.scope
+          );
+        })
+        .catch((error) => {
+          console.error('Service Worker registration failed:', error);
+        });
+    }
+  }, []);
+
   return (
     <>
       <Head>
-        <link
-          rel="preload"
-          href="/fonts/LexendMedium.ttf"
-          as="font"
-          crossOrigin="anonymous"
-          type="font/ttf"
-        />
-        <link
-          rel="preload"
-          href="/fonts/LexendBold.ttf"
-          as="font"
-          crossOrigin="anonymous"
-          type="font/ttf"
-        />
-        <link
-          rel="preload"
-          href="/fonts/LexendRegular.ttf"
-          as="font"
-          crossOrigin="anonymous"
-          type="font/ttf"
-        />
-        <link
-          rel="preload"
-          href="/fonts/LexendLight.ttf"
-          as="font"
-          crossOrigin="anonymous"
-          type="font/ttf"
-        />
-        <title>Welcome to blog!</title>
+        <meta title="App" />
       </Head>
       <ThemeProvider>
         <Component {...pageProps} />
