27c119fcc6ef833f10e6cc962b37432b19aa36bb
Adrian Połubiński <adrian.polubinski@billennium.com>
2023-09-29 13:04:22 +0200

    Fix fonts

--- system/libs/figa-ui/.storybook/preview.tsx
+++ system/libs/figa-ui/.storybook/preview.tsx
@@ -2,6 +2,7 @@ import type { DecoratorFunction } from '@storybook/addons';
 import type { Story } from '@storybook/react';
 import { ThemeProvider } from '../src/lib/theme-provider';
 import { Sandbox } from '../src/lib/sandbox';
+import '../src/index.css';
 
 const withGlobalStyleDecorator: DecoratorFunction<JSX.Element> = (
   StoryComponent: Story
