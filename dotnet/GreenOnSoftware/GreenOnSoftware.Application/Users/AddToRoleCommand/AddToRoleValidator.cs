1670d9c579caf992a18809192549c808b53c4f72
Artur Węgrodzki <artur.wegrodzki@gmail.com>
2023-09-29 12:11:21 +0200

    Allow assign and remove to Admin role

--- dotnet/GreenOnSoftware/GreenOnSoftware.Application/Users/AddToRoleCommand/AddToRoleValidator.cs
+++ dotnet/GreenOnSoftware/GreenOnSoftware.Application/Users/AddToRoleCommand/AddToRoleValidator.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using GreenOnSoftware.Commons.Consts;
+using GreenOnSoftware.Commons.Helpers;
 
 namespace GreenOnSoftware.Application.Users.AddToRoleCommand;
 
@@ -11,8 +12,11 @@ public class AddToRoleValidator : AbstractValidator<AddToRole>
             .Must(x => Role.All.Contains(x))
             .WithMessage("Role must have valid value");
 
-        RuleFor(x => x.Role)
-            .Must(x => x.ToLower() != Role.Admin.ToLower())
-            .WithMessage("Administrators cannot be change");
+        if (!EnvironmentHelper.IsDevOrLocal())
+        {
+            RuleFor(x => x.Role)
+                .Must(x => x.ToLower() != Role.Admin.ToLower())
+                .WithMessage("Administrators cannot be change");
+        }
     }
 }
