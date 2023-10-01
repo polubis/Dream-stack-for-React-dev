c17902c9d463b8b3cc002e25ca31c433d40acb69
hetres122 <hetres122@gmail.com>
2023-10-01 16:49:45 +0200

    After sign out move user to articles page

--- system/apps/blog/core/auth/use-auth.ts
+++ system/apps/blog/core/auth/use-auth.ts
@@ -1,9 +1,13 @@
 import { unauthorize, useAuthStore } from '../../store/auth';
 import { interceptUnauthorized } from '@system/blog-api';
 import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { useLang } from '../../dk';
 
 const useAuth = () => {
   const authStore = useAuthStore();
+  const router = useRouter();
+  const lang = useLang();
 
   useEffect(() => {
     const { listen, clean } = interceptUnauthorized(unauthorize);
@@ -12,11 +16,15 @@ const useAuth = () => {
 
     authStore.check();
 
+    if (authStore.key === 'unauthorized') {
+      router.push(`/${lang}/articles`);
+    }
+
     return () => {
       clean();
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [authStore.key]);
 };
 
 export { useAuth };
