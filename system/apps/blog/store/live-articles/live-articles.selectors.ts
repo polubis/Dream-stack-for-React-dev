a3ddcc42e2e548b0e4a9f24eb262d58811309bff
Adrian Połubiński <polubis@wp.pl>
2023-10-17 08:12:07 +0200

    232. [PATCH] Add missing live-articles features (#321)

--- system/apps/blog/store/live-articles/live-articles.selectors.ts
+++ system/apps/blog/store/live-articles/live-articles.selectors.ts
@@ -7,16 +7,25 @@ const checkHasLoadedAll = ({
   currentPage,
 }: GetArticlesResponse): boolean => currentPage >= totalPages;
 
+const isSafeState = (
+  state: LiveArticlesStore.State
+): state is LiveArticlesStore.SafeState =>
+  state.is !== 'idle' && state.is !== 'fail';
+
 export const live_articles_selectors: LiveArticlesStore.Selectors = {
   safeState: () => {
     const state = useLiveArticlesStore.getState();
 
-    if (state.is === 'idle' || state.is === 'fail') {
-      throw Error('Invalid read attempt for state ' + state.is);
+    if (isSafeState(state)) {
+      return state;
     }
 
-    return state;
+    throw Error('Invalid read attempt for state ' + state.is);
   },
   allLoaded: () =>
     checkHasLoadedAll(live_articles_selectors.safeState().response),
+  getSearch: () => {
+    const state = useLiveArticlesStore.getState();
+    return isSafeState(state) ? state.params.Search : '';
+  },
 };
