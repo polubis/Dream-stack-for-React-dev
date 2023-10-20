a308dbd09bc7317505c09e517e7eb38747ce50a4
polubis <polubis@wp.pl>
2023-10-20 08:38:31 +0200

    wip

--- system/libs/blog-api/src/lib/index.ts
+++ system/libs/blog-api/src/lib/index.ts
@@ -1,4 +1,5 @@
 export * from './methods';
 export * from './interceptors';
 export * from './test-utils';
+export * from './guards';
 export { getErrors, getError, getPath, createError } from './core';
