ecbb204833b881f4a589ca7529627f19e3de9fc0
Adrian Połubiński <adrian.polubinski@billennium.com>
2023-10-30 09:59:40 +0100

    wip

--- system/apps/blog/components/article-reviews-list/article-reviews-list.tsx
+++ system/apps/blog/components/article-reviews-list/article-reviews-list.tsx
@@ -3,8 +3,10 @@ import {
   Box,
   Button,
   CloseIcon,
+  Field,
   Font,
   Loader,
+  Textarea,
   column,
   row,
   tokens,
@@ -14,6 +16,12 @@ import styled from 'styled-components';
 import { InfoSection } from '../info-section';
 import type { ArticleReviewsListProps } from './defs';
 import { AdminsOnly } from '../../core';
+import {
+  add_article_review_actions,
+  useAddArticleReviewStore,
+} from '../../store/add-article-review';
+import { article_management_actions } from '../../store/article-management';
+import { article_selectors, useArticleStore } from '../../store/article';
 
 const Container = styled.ul`
   ${column()}
@@ -36,17 +44,18 @@ const Container = styled.ul`
 `;
 
 const ArticleReviewsList = ({ onClose }: ArticleReviewsListProps) => {
+  const article = article_selectors.useArticle();
   const articleReviewsStore = useArticleReviewsStore();
-  const { is } = articleReviewsStore;
+  const addArticleReviewStore = useAddArticleReviewStore();
 
-  if (is === 'idle' || is === 'busy')
+  if (articleReviewsStore.is === 'idle' || articleReviewsStore.is === 'busy')
     return (
       <Box margin="auto">
         <Loader />
       </Box>
     );
 
-  if (is === 'fail')
+  if (articleReviewsStore.is === 'fail')
     return (
       <InfoSection
         padding={[0, 0, 0, 0]}
@@ -56,18 +65,37 @@ const ArticleReviewsList = ({ onClose }: ArticleReviewsListProps) => {
       />
     );
 
-  const { reviews } = articleReviewsStore;
-
-  const AddReviewButton = (
+  const AddReviewSection = (
     <AdminsOnly>
-      <Button size={2}>Add review</Button>
+      <Box spacing={[250]} padding={[0, 50, 0, 50]}>
+        <Field
+          label="Review*"
+          hint="Remember to make review understable for readers"
+        >
+          <Textarea
+            value={addArticleReviewStore.form.values.content}
+            onChange={(e) =>
+              add_article_review_actions.setField('content', e.target.value)
+            }
+          />
+        </Field>
+        <Button
+          onClick={() => article_management_actions.confirm(article.id)}
+          loading={addArticleReviewStore.is === 'busy'}
+          size={2}
+        >
+          Comment
+        </Button>
+      </Box>
     </AdminsOnly>
   );
 
   return (
     <Box spacing={[200]}>
       <Box orientation="row" between>
-        <Font variant="h6">Reviews {`(${reviews.length})`}</Font>
+        <Font variant="h6">
+          Reviews {`(${articleReviewsStore.reviews.length})`}
+        </Font>
         <Button
           size={1}
           shape="rounded"
@@ -78,30 +106,32 @@ const ArticleReviewsList = ({ onClose }: ArticleReviewsListProps) => {
           <CloseIcon />
         </Button>
       </Box>
-      {reviews.length > 0 ? (
+      {articleReviewsStore.reviews.length > 0 ? (
         <Container className="reviews-list">
-          {reviews.map(({ id, reviewerName, createdDate }) => (
-            <li className="review-list-item" key={id}>
-              <div>
-                <Avatar
-                  alt={reviewerName}
-                  src="https://media-cldnry.s-nbcnews.com/image/upload/rockcms/2022-08/220805-domestic-cat-mjf-1540-382ba2.jpg"
-                />
+          {articleReviewsStore.reviews.map(
+            ({ id, reviewerName, createdDate }) => (
+              <li className="review-list-item" key={id}>
                 <div>
-                  <Font variant="b3">{createdDate}</Font>
-                  <Font variant="b2">{reviewerName}</Font>
-                  {/* @TODO: Ask backend for content model and author avatar src. */}
-                  <Font variant="b1">Some content</Font>
+                  <Avatar
+                    alt={reviewerName}
+                    src="https://media-cldnry.s-nbcnews.com/image/upload/rockcms/2022-08/220805-domestic-cat-mjf-1540-382ba2.jpg"
+                  />
+                  <div>
+                    <Font variant="b3">{createdDate}</Font>
+                    <Font variant="b2">{reviewerName}</Font>
+                    {/* @TODO: Ask backend for content model and author avatar src. */}
+                    <Font variant="b1">Some content</Font>
+                  </div>
                 </div>
-              </div>
-            </li>
-          ))}
-          {AddReviewButton}
+              </li>
+            )
+          )}
+          {AddReviewSection}
         </Container>
       ) : (
         <Box spacing={[200]}>
           <Font variant="b2">No reviews added yet</Font>
-          {AddReviewButton}
+          {AddReviewSection}
         </Box>
       )}
     </Box>
