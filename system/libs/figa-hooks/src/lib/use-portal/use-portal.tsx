cd2af9a43137b8d7059c43971bd414082658230b
Adrian Połubiński <adrian.polubinski@billennium.com>
2023-11-08 11:55:48 +0100

    [PATCH] 250. Improve usePortal hook

--- system/libs/figa-hooks/src/lib/use-portal/use-portal.tsx
+++ system/libs/figa-hooks/src/lib/use-portal/use-portal.tsx
@@ -1,27 +1,27 @@
-import { useEffect, useMemo } from 'react';
+import { useMemo } from 'react';
 import { createPortal } from 'react-dom';
 import type { UsePortal } from './defs';
+import { useIsomorphicLayoutEffect } from '../use-isomorphic-layout-effect';
+import { isServer } from '@system/utils';
 
-// usePortal is an implementation of the facade pattern.
 const usePortal: UsePortal = () => {
-  // Creates only one instance of div.
-  const wrapper = useMemo(() => document.createElement('div'), []);
+  const wrapper = useMemo(
+    () => (isServer() ? null : document.createElement('div')),
+    []
+  );
 
-  useEffect(() => {
-    // Adds div tag to body.
+  useIsomorphicLayoutEffect(() => {
+    if (!wrapper) return;
+
     document.body.appendChild(wrapper);
 
     return () => {
-      // After unmounting the component - removes the div created earlier.
       document.body.removeChild(wrapper);
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  // Returns an object with function that will allow you to use the portal.
   return {
-    // This anonymous function is an implementation of the factory method pattern.
-    render: (children) => createPortal(children, wrapper),
+    render: (children) => (wrapper ? createPortal(children, wrapper) : null),
   };
 };
 
