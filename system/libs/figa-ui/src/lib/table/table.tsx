2f2e282e4c34c12369c63c89fb1d5151552a58a0
Szymon Taranczewski <sz.taranczewski@gmail.com>
2023-11-14 22:50:57 +0100

    wip

--- system/libs/figa-ui/src/lib/table/table.tsx
+++ system/libs/figa-ui/src/lib/table/table.tsx
@@ -1,42 +1,53 @@
-import { useState, useReducer } from 'react';
+import { useReducer } from 'react';
 import { TableProps, RowData, TableState, TableAction } from './defs';
 import { ROWS_PER_PAGE } from './consts';
 import styled from 'styled-components';
+import { Font } from '../font';
+import { Button } from '../button';
 
 const TableWrapper = styled.div`
   overflow: auto;
   max-width: 100%;
+
+  table {
+    width: 100%;
+    border-collapse: collapse;
+  }
 `;
 
-const TableHeader = styled.div`
-  display: flex;
-  background-color: #f0f0f0;
-  font-weight: bold;
-`;
+// const TableHeader = styled.thead`
+//   display: flex;
+//   background-color: #f0f0f0;
+//   font-weight: bold;
+//   font: th {
+//     flex: 1;
+//     padding: 8px;
+//   }
+// `;
 
-const TableCell = styled.div`
-  flex: 1;
-  padding: 8px;
-`;
+// const TableCell = styled.td`
+//   flex: 1;
+//   padding: 8px;
+// `;
 
-const EditButton = styled.button`
-  background-color: #007bff;
-  color: #fff;
-  border: none;
-  padding: 6px 12px;
-  cursor: pointer;
-`;
+// const EditButton = styled.button`
+//   background-color: #007bff;
+//   color: #fff;
+//   border: none;
+//   padding: 6px 12px;
+//   cursor: pointer;
+// `;
 
-const EditWindow = styled.div`
-  background-color: #fff;
-  border: 1px solid #ccc;
-  padding: 12px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  z-index: 1;
-`;
+// const EditWindow = styled.div`
+//   background-color: #fff;
+//   border: 1px solid #ccc;
+//   padding: 12px;
+//   position: absolute;
+//   top: 50%;
+//   left: 50%;
+//   transform: translate(-50%, -50%);
+//   z-index: 1;
+// `;
 
 const tableReducer = (state: TableState, action: TableAction): TableState => {
   switch (action.type) {
@@ -56,7 +67,12 @@ const initialState = {
   rowsPerPage: 10,
 };
 
-const Table = ({ data, columns, onPageChange, onRowsPerPageChange }: TableProps<RowData>) => {
+const Table = ({
+  data,
+  columns,
+  onPageChange,
+  onRowsPerPageChange,
+}: TableProps<RowData>) => {
   const [tableState, dispatch] = useReducer(tableReducer, initialState);
   const { page, rowsPerPage } = tableState;
 
@@ -73,7 +89,9 @@ const Table = ({ data, columns, onPageChange, onRowsPerPageChange }: TableProps<
   };
 
   const handleRowsPerPageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const selectedValue = Number(e.target.value) as typeof ROWS_PER_PAGE[number];
+    const selectedValue = Number(
+      e.target.value
+    ) as (typeof ROWS_PER_PAGE)[number];
     dispatch({ type: 'SET_ROWS_PER_PAGE', payload: selectedValue });
     onRowsPerPageChange(selectedValue);
   };
@@ -138,13 +156,29 @@ const Table = ({ data, columns, onPageChange, onRowsPerPageChange }: TableProps<
               <td>
                 {row.isEditing ? (
                   <>
-                    <button onClick={() => confirmEdit(row)}>Confirm</button>
-                    <button onClick={() => cancelEdit(row)}>Cancel</button>
+                    <Button
+                      shape="rectangle"
+                      size={1}
+                      onClick={() => confirmEdit(row)}
+                    >
+                      Confirm
+                    </Button>
+                    <Button
+                      shape="rectangle"
+                      size={1}
+                      onClick={() => cancelEdit(row)}
+                    >
+                      Cancel
+                    </Button>
                   </>
                 ) : (
-                  <EditButton onClick={() => toggleEdit(row)}>
+                  <Button
+                    shape="rectangle"
+                    size={1}
+                    onClick={() => toggleEdit(row)}
+                  >
                     {row.isEditing ? 'Save' : 'Edit'}
-                  </EditButton>
+                  </Button>
                 )}
               </td>
             </tr>
@@ -153,15 +187,22 @@ const Table = ({ data, columns, onPageChange, onRowsPerPageChange }: TableProps<
       </table>
 
       <div>
-        <button onClick={prevPage} disabled={page === 1}>
+        <Button
+          shape="rectangle"
+          size={1}
+          onClick={prevPage}
+          disabled={page === 1}
+        >
           Prev
-        </button>
-        <button
+        </Button>
+        <Button
+          shape="rectangle"
+          size={1}
           onClick={nextPage}
           disabled={page === Math.ceil(data.length / rowsPerPage)}
         >
           Next
-        </button>
+        </Button>
         <select value={rowsPerPage} onChange={handleRowsPerPageChange}>
           {[10, 15, 20, 30].map((value) => (
             <option key={value} value={value}>
