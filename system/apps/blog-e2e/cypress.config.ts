e97473c163a9edc304fff391c9d26db2a08f91ae
Adrian Połubiński <adrian.polubinski@billennium.com>
2023-11-17 08:43:08 +0100

    wip

--- system/apps/blog-e2e/cypress.config.ts
+++ system/apps/blog-e2e/cypress.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from 'cypress';
 import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
 
 export default defineConfig({
-  e2e: {
-    ...nxE2EPreset(__dirname),
-  },
+  e2e: nxE2EPreset(__dirname),
   env: {
     NEXT_PUBLIC_API_URL: process.env['NEXT_PUBLIC_API_URL'],
   },
--- system/apps/blog-e2e/src/e2e/test.cy.ts
+++
@@ -1,9 +0,0 @@
-import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
-
-Given('Im on articles page', () => {
-  cy.visit('/en/articles-creator?url=my-new-best-article');
-});
-
-Then('I see article', () => {
-  cy.get('a');
-});
