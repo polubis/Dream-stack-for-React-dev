6fd580ddfd13427beb450de4a44cb1d2cc6db6f4
Adrian Połubiński <adrian.polubinski@billennium.com>
2023-12-06 12:19:59 +0100

    wip

--- system/apps/blog/components/main-layout/right-bar.tsx
+++ system/apps/blog/components/main-layout/right-bar.tsx
@@ -1,7 +1,12 @@
+import { useScroll } from '@system/figa-hooks';
 import { Bar, type BarProps } from '@system/figa-ui';
 
-const RightBar = (props: Omit<BarProps, 'right' | 'left'>) => (
-  <Bar {...props} right />
-);
+const RightBar = (props: Omit<BarProps, 'right' | 'left'>) => {
+  const [scroll] = useScroll({ delay: 50 });
+
+  return scroll.is === 'progress' && scroll.value < 94 ? (
+    <Bar {...props} right />
+  ) : null;
+};
 
 export { RightBar };
