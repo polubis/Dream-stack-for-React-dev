79a8caface0f4b63dae6a5f5d54a7caf8138ec3e
Adrian Połubiński <adrian.polubinski@billennium.com>
2023-12-06 11:51:58 +0100

    wip

--- system/libs/figa-ui/src/lib/bottom-nav-item/defs.ts
+++ system/libs/figa-ui/src/lib/bottom-nav-item/defs.ts
@@ -3,7 +3,6 @@ import { ButtonProps } from '../button';
 
 interface BottomNavItemProps
   extends Omit<ButtonProps, 'children' | 'variant' | 'motive'> {
-  className?: string;
   text: string;
   icon: ReactNode;
   active?: boolean;
